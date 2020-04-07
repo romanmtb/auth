77e0269a7607cbe8d270c89b3cd20fa6d40dc01c
roman <r.alexeychenko@mmdsmart.com>
2020-04-07 19:19:59 +0300

    add router, routes

--- client/src/core/Layout.js
+++ client/src/core/Layout.js
@@ -1,13 +1,14 @@
 import React, {Fragment} from 'react'
+import {Link} from 'react-router-dom'
 
 const Layout = ({children}) => {
 
     const nav = () => (
         <ul className="nav nav-tabs bg-primary">
             <li className="nav-item">
-                <a href="/" className="text-light nav-link">
+                <Link href="/" to={'/'} className="text-light nav-link">
                     Home
-                </a>
+                </Link>
             </li>
         </ul>
     )
