633361c12026aa6a2661e5de3557511137854069
roman <r.alexeychenko@mmdsmart.com>
2020-04-07 19:56:32 +0300

    add sign up page layout

--- client/src/auth/Signup.js
+++ client/src/auth/Signup.js
@@ -6,11 +6,55 @@ import {ToastContainer, toas} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.min.css'
 
 const Signup = () => {
+    const [values, setValues] = useState({
+        name: 'Roman',
+        email: 'roman.alexeychenko@gmail.com',
+        password: 'rrrrrr',
+        buttonText: 'Submit'
+    })
+
+    const handleChange = name => event => {
+
+    }
+
+    const clickSubmit = event => {
+
+    }
+
+    const {name, email, password, buttonText} = values
+
+    const signupForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input onChange={handleChange('name')} value={name} type="text" className="form-control"/>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input onChange={handleChange('email')} value={email} type="email" className="form-control"/>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input onChange={handleChange('password')} value={password} type="password" className="form-control"/>
+            </div>
+
+            <div>
+                <button onClick={clickSubmit} className="btn btn-primary">
+                    {buttonText}
+                </button>
+            </div>
+        </form>
+    )
+
     return (
         <Layout>
-            <ToastContainer />
-
-            <h1>Signup</h1>
+            <div className="col-md-6 offset-md-3">
+                <ToastContainer />
+                <h1 className="p-5 text-center">Signup</h1>
+                {signupForm()}
+            </div>
         </Layout>
     )
 }
