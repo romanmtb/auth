e6670cdc7cc47d60cc38d3ae6fcd6c11b6bfd48d
roman <r.alexeychenko@mmdsmart.com>
2020-04-07 20:17:24 +0300

    add signup form requests

--- client/src/auth/Signup.js
+++ client/src/auth/Signup.js
@@ -2,8 +2,9 @@ import React, {useState} from 'react'
 import {Link, Redirect} from 'react-router-dom'
 import Layout from '../core/Layout'
 import axios from 'axios'
-import {ToastContainer, toas} from 'react-toastify'
+import {ToastContainer, toast} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.min.css'
+import {resolveToLocation} from 'react-router-dom/modules/utils/locationUtils'
 
 const Signup = () => {
     const [values, setValues] = useState({
@@ -14,11 +15,27 @@ const Signup = () => {
     })
 
     const handleChange = name => event => {
-
+        setValues({...values, [name]: event.target.value})
     }
 
     const clickSubmit = event => {
-
+        event.preventDefault()
+        setValues({...values, buttonText: 'Submitting'})
+        axios({
+            method: 'POST',
+            url: `${process.env.REACT_APP_API}/signup`,
+            data: {name, email, password}
+        })
+            .then(response => {
+                console.log('SIGNUP SUCCESS', response)
+                setValues({...values, name: '', email: '', password: '', buttonText: 'Submitted'})
+                toast.success(response.data.message)
+            })
+            .catch(error => {
+                console.log('SIGNUP ERROR', error.response.data)
+                setValues({...values, buttonText: 'Submit'})
+                toast.error(error.response.data.error)
+            })
     }
 
     const {name, email, password, buttonText} = values
