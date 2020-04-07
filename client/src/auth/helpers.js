fc9304be97c0bde049dbe8eb866f370fe79a7bb2
roman <r.alexeychenko@mmdsmart.com>
2020-04-08 01:12:43 +0300

    add helpers get, set, remove from cookie, localstorage

---
+++ client/src/auth/helpers.js
@@ -0,0 +1,59 @@
+import cookie from 'js-cookie'
+
+export const setCookie = (key, value) => {
+    if (!!window) {
+        cookie.set(key, value, {
+            expires: 1
+        })
+    }
+}
+
+export const removeCookie = key => {
+    if (!!window) {
+        cookie.remove(key, {
+            expires: 1
+        })
+    }
+}
+
+export const getCookie = key => {
+    if (!!window) {
+        return cookie.get(key)
+    }
+}
+
+export const setLocalStorage = (key, value) => {
+    if (!!window) {
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+}
+
+export const removeLocalStorage = key => {
+    if (!!window) {
+        localStorage.removeItem(key)
+    }
+}
+
+export const authenticate = (response, next) => {
+    console.log('AUTHENTICATE HELPER ON SIGNIN RESPONSE')
+    setCookie('token', response.data.token)
+    setCookie('user', response.data.user)
+    next()
+}
+
+export const isAuth = () => {
+    if (!!window) {
+
+        const cookieChecked = getCookie('token')
+
+        if (cookieChecked) {
+            if (localStorage.getItem('user')) {
+                return JSON.parse(localStorage.getItem('user'))
+            } else {
+                return false
+            }
+        }
+
+    }
+}
+
